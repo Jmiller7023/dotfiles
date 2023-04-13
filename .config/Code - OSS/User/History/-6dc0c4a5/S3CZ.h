019f9e6235a9332b7812c8c8020948bfde0dbf4e
Joseph Miller <fireyshyguy@gmail.com>
2023-04-13 14:11:45 -0700

    base configuration

---
+++ .config/Code - OSS/User/History/-6dc0c4a5/S3CZ.h
@@ -0,0 +1,10 @@
+#ifndef SHIFTVIEW_DEFINED
+#define SHIFTVIEW_DEFINED
+/** Function to shift the current view to the left/right
+ *
+ * @param: "arg->i" stores the number of tags to shift right (positive value)
+ *          or left (negative value)
+ */
+void
+shiftview(const Arg *arg);
+#endif //SHIFTVIEW_DEFINED
